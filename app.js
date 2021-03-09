e1743cca357fcc57f347ad67107556ecf8219cb0
KuldeepKumarBilhatiya <kd141990@gmail.com>
2021-03-09 20:47:43 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+// var arrayData = new Array("Kuldeep","Abhishek","Pankaj",30,29,31);
+// for (let index = 0; index < arrayData.length; index++) {
+
+//     console.log(arrayData[index]);
+    
+// }
+
+// var arrayElement = prompt("Enter No. Of Elements");
+// var arrayData = [];
+
+// for(let i = 0; i<=arrayElement; i++){
+//     arrayData[i] = prompt("Enter Name");
+// }
+
+// for(let i = 0; i <= arrayElement; i++){
+//     document.write(arrayData[i] + "<br>");
+//     //console.log(arrayData[i]);
+// }
+
+
+
+// var person = {fname:"John", lname:"Doe", age:25};
+// var txt = '';
+// for (x in person) {
+//   console.log(person[x]);
+// }
+
+
+var name = "Kuldeep Kumar";
+//console.log(`Hello ${name}`);
+
+
+
+
+
+
+
